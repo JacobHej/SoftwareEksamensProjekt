9f629f19bc2a6f15b0c9d88af4676bc8700e0e24
TommyJonasRath <s194586@student.dtu.dk>
2020-04-30 16:08:18 +0200

    Update aktivitetSteps.java
    feature: leder opretter aktivitet
    (ikke faerdig)

--- EksamensProjekt/src/test/java/tests/aktivitetSteps.java
+++ EksamensProjekt/src/test/java/tests/aktivitetSteps.java
@@ -1,23 +1,29 @@
 package test.java.tests;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import Applikationslag.Domaeneklasser.*;
+import java.util.Date;
 
 public class aktivitetSteps {
+	Projekt projekt;
 	
 	@Given("der er et projekt med lederen leder med idet id {string}")
 	public void derErEtProjektMedLederenLederMedIdetId(String string) {
-	    //assertTrue(Projekt.Leder() == string));
-		assertFalse(false);
+	    //opret projekt med leder "string";
+		Medarbejder leder = new Medarbejder();
+		Date startDate = new Date();
+		Projekt projekt = new Projekt("string", startDate, leder);
+		assertTrue(true); //¯\_(ツ)_/¯
 	}
 	
 	@When("lederen laver en ny aktivitet med navnet {string}")
 	public void lederenLaverEnNyAktivitetMedNavnet(String string) {
-		//assertFalse(aktivitet med navn string findes allerede)
-		//**Opret ny aktivitet**
+		assertTrue(projekt.tilføjAktivitet(new Aktivitet(string,projekt)));
 	    throw new io.cucumber.java.PendingException();
 	}
 	
