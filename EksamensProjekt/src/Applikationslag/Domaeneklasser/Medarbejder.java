c53641bc76ee726e17e2192409022d6082ef5cb1
JacobHej <jacob@hejlsberg.dk>
2020-04-30 16:57:55 +0200

    navngivning

--- EksamensProjekt/src/Applikationslag/Domaeneklasser/Medarbejder.java
+++ EksamensProjekt/src/Applikationslag/Domaeneklasser/Medarbejder.java
@@ -6,18 +6,21 @@ public class Medarbejder
 {
 	// klassevariable
     private UUID id = UUID.randomUUID();
-    private String name;
+    private String navn;
     
     //metoder
-    
+    public Medarbejder(String navn)
+    {
+    	this.navn = navn;
+    }
     //Funtioner for returnering af klassevariable
     public UUID ID()
     {
     	return this.id;
     }
     
-    public String Name()
+    public String Navn()
     {
-    	return this.name;
+    	return this.navn;
     }
 }
