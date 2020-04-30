20b3ad19593a5fbbd96f7d857715a1d54e1e1ad3
JacobHej <jacob@hejlsberg.dk>
2020-04-30 13:43:15 +0200

    Setup Projekt og rapport uden implementation

--- EksamensProjekt/src/Applikationslag/Domaeneklasser/Projekt.java
+++ EksamensProjekt/src/Applikationslag/Domaeneklasser/Projekt.java
@@ -1,5 +1,63 @@
 package Applikationslag.Domaeneklasser;
 
-public class Projekt {
+import java.util.Date;
+import java.util.UUID;
 
+public class Projekt {
+	private UUID id = UUID.randomUUID();
+	private Medarbejder leder;
+	private String navn;
+	private Date startTid;
+	
+	public Projekt(String navn)
+	{
+		
+	}
+	
+	public Projekt(String navn, Medarbejder leder)
+	{
+		
+	}
+	
+	public void TilfoejAktivitet(Aktivitet aktivitet)
+	{
+		
+	}
+	
+	public void TilfoejAktivitet(Date start, Date slut, Medarbejder medarbejder)
+	
+	public Rapport GenererRapport()
+	{
+		
+	}
+	
+	public void SaetLeder(Medarbejder leder)
+	{
+		this.leder = leder;
+	}
+	
+	public SaetStartTid(Date startTid)
+	{
+		this.startTid = startTid;
+	}
+	
+	public UUID ID()
+	{
+		return this.id;
+	}
+	
+	public Medarbejder Leder()
+	{
+		return this.leder;
+	}
+	
+	public String Navn()
+	{
+		return this.navn;
+	}
+
+	public Date StartTid()
+	{
+		return this.startTid;
+	}
 }
