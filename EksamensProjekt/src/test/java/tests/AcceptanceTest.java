9b9ad7cb48673be1a1f31aee0e9f540d6a6506a6
KafferAffer <46445011+KafferAffer@users.noreply.github.com>
2020-04-30 13:39:59 +0200

    Corrected Glue
    Made cucumber work correctly

--- EksamensProjekt/src/test/java/tests/AcceptanceTest.java
+++ EksamensProjekt/src/test/java/tests/AcceptanceTest.java
@@ -12,7 +12,7 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 	plugin = { "html:target/cucumber/wikipedia.html"}, 
 	monochrome=true, 
 	snippets = SnippetType.CAMELCASE, 
-	glue = { "tests"},
+	glue = { "test.java.tests"},
 	strict = true)
 public class AcceptanceTest {
 
