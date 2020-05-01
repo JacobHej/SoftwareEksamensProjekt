b91a5123d842606ff9a3fc899a676ca12c396f33
TommyJonasRath <s194586@student.dtu.dk>
2020-05-01 22:54:19 +0200

    Steps

--- EksamensProjekt/src/test/java/tests/aktivitetSteps.java
+++ EksamensProjekt/src/test/java/tests/aktivitetSteps.java
@@ -14,56 +14,96 @@ import Applikationslag.Redskaber.Managers;
 import java.util.Date;
 
 public class aktivitetSteps {
-	static Medarbejder leder;
-	static Projekt projekt;
-	static Aktivitet aktivitet;
-	static Date startdate;
-	String aktivitetID;
-	String projektNavn;
+	Medarbejder leder;
+	Medarbejder medarbejder;
+	Projekt projekt;
+	Aktivitet aktivitet;
 	IAktivitetManager aktivitetManager= Managers.FaaAktivitetManager();
 	
 	
 	@Given("der er et projekt {string} med lederen {string}")
-	public void derErEtProjektMedLederen(String projektID, String lederID) {
-		this.projektNavn=projektNavn;
-		leder = new Medarbejder(lederID);
-		projekt = new Projekt(projektNavn);
-		projekt.Gem();
+	public void derErEtProjektMedLederen(String projektNavn, String lederID) {
+		//this.projektNavn=projektNavn;
+		this.leder = new Medarbejder(lederID);
+		this.projekt = new Projekt(projektNavn);
+		this.projekt.Gem();
 		//¯\_(ツ)_/¯
 	}
 
 	@When("lederen laver en ny aktivitet med navnet {string}")
 	public void lederenLaverEnNyAktivitetMedNavnet(String aktivitetID) {
-		aktivitet= new Aktivitet(aktivitetID);
-		assertTrue(projekt.tilfoejAktivitet(aktivitet));
+		this.aktivitet= new Aktivitet(aktivitetID);
+		assertTrue(this.projekt.tilfoejAktivitet(aktivitet));
 	}
 	
 	@Then("projektet {string} har en ny aktivitet med navnet {string}")
 	public void projektetHarEnNyAktivitetMedNavnet(String projektNavn, String aktivitetID) {
-		assertTrue(aktivitetManager.eksisterer(aktivitet));
-		assertTrue(aktivitet.getProjekt() == projekt);
+		assertTrue(aktivitetManager.eksisterer(this.aktivitet));
+		assertTrue(this.aktivitet.getProjekt() == this.projekt);
 	}
 	
 	
 	// Forsoeger at tilfoeje aktivitet som allerede findes i projektet
 	@Given("Projektet {string} har aktiviteten {string}")
-	public void projektetHarAktiviteten(String ProjektNavn, String aktivitetID) {
-	    projekt = new Projekt(projektNavn);
-	    projekt.Gem();										//opret projekt
-	    aktivitet = new Aktivitet(aktivitetID);				//opret aktivitet
-	    projekt.tilfoejAktivitet(aktivitet);				//tilfoej aktivitet til projekt
-		assertTrue(aktivitetManager.eksisterer(aktivitet));	//tjek at aktiviteten findes
-		assertTrue(aktivitet.getProjekt() == projekt);		//tjek at aktivitet er i projekt
+	public void projektetHarAktiviteten(String projektNavn, String aktivitetID) {
+	    this.projekt = new Projekt(projektNavn);
+	    this.projekt.Gem();										//opret projekt
+	    this.aktivitet = new Aktivitet(aktivitetID);				//opret aktivitet
+	    this.projekt.tilfoejAktivitet(this.aktivitet);				//tilfoej aktivitet til projekt
+		assertTrue(aktivitetManager.eksisterer(this.aktivitet));	//tjek at aktiviteten findes
+		assertTrue(this.aktivitet.getProjekt() == this.projekt);		//tjek at aktivitet er i projekt
 	}
 
 	@When("lederen proever at lave en ny aktivitet med navnet {string}")
 	public void lederenProeverAtLaveEnNyAktivitetMedNavnet(String string) {
-		assertFalse(projekt.tilfoejAktivitet(aktivitet));
+		assertFalse(this.projekt.tilfoejAktivitet(this.aktivitet));
 	}
 
 	@Then("Kast exception {string}")
 	public void kastException(String ErrorMessage) {
 //	    throw new IllegalArgumentException(ErrorMessage);
 	}
+	
+// 	Medarbejder tilfoejer tid til en aktivitet.
+	@Given("der er en medarbejder med ID'et {string} og et projekt med navnet {string}")
+	public void derErEnMedarbejderMedIDEtOgEtProjektMedNavnet(String MedarbejderID, String ProjektNavn) {
+	    this.medarbejder = new Medarbejder (MedarbejderID);
+	    this.medarbejder.Gem();
+	    this.projekt= new Projekt (ProjektNavn);
+	    this.projekt.Gem();
+	}
+
+	@Given("Projektet {string} har en aktivitet {string}")
+	public void projektetHarEnAktivitet(String ProjektNavn, String AktivitetID) {
+	    this.aktivitet = new Aktivitet (AktivitetID);
+	    assertTrue(this.projekt.tilfoejAktivitet(this.aktivitet));
+	}
+	
+	@Given("Medarbejderen {string} arbejder paa aktiviteten {string}")
+	public void medarbejderenArbejderPaaAktiviteten(String MedarbejderID, String AktivitetID) {
+		System.out.println(this.aktivitet.SaetMedarbejder(this.medarbejder));
+//	    this.aktivitet.SaetMedarbejder(this.medarbejder);
+//	    System.out.println(this.aktivitet.Medarbejder());
+	    
+	} 
+//	
+//	@Given("Aktiviteten {string} har mere end {int} ubrugte budgetterede timer")
+//	public void aktivitetenHarMereEndUbrugteBudgetteredeTimer(String string, Integer int1) {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//	
+//	@When("Medarbejderen {string} tilfoejer {string} timer til aktiviteten {string}")
+//	public void medarbejderenTilfoejerTimerTilAktiviteten(String string, String string2, String string3) {
+//	    // this.aktivitet.TilfoejTid(tid, medarbejder)
+//	    throw new io.cucumber.java.PendingException();
+//	}
+
+
+//	@Then("Medarbejderens timer er blevet noteret")
+//	public void medarbejderensTimerErBlevetNoteret() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
 	
 }
---
+++ EksamensProjekt/use_cases/features/tilfoej_tid_til_aktivitet.feature
@@ -0,0 +1,18 @@
+Feature: tilfoej tid til aktivitet
+  Description: Tilfoej tid til en aktivitet
+	Actor: Medarbejder
+
+Scenario: Medarbejder tilfoejer arbejdstimer til aktivitet som har nok budgetteret tid
+    Given der er en medarbejder med ID'et "MedarbejderID" og et projekt med navnet "ProjektNavn"
+    And Projektet "ProjektNavn" har en aktivitet "AktivitetID"
+    And Medarbejderen "MedarbejderID" arbejder paa aktiviteten "AktivitetID"
+    And Aktiviteten "AktivitetID" har mere end 2 ubrugte budgetterede timer
+    When Medarbejderen "MedarbejderID" tilfoejer "2" timer til aktiviteten "AktivitetID"
+    Then Medarbejderens timer er blevet noteret
+    
+
+#Scenario: Medarbejder tilfoejer flere timer end der er bugetteret for paa aktiviteten.
+#	Given der er en medarbejder med ID'et "MedarbejderID" og et projekt med ID'et "ProjektID"
+#	And Projektet "ProjektID" har en aktivitet "AktivitetID"
+#	When Medarbejderen "MedarbejderID" tilfoejer for mange timer til aktiviteten "AktivitetID"
+#	Then Kast exception "Antallet af timer kan ikke registreres - overgaar budgetteret tid!"
