da745828939a595621283eefbdfa57511ff7bda3
TommyJonasRath <s194586@student.dtu.dk>
2020-05-01 17:01:15 +0200

    features

--- EksamensProjekt/src/test/java/tests/aktivitetSteps.java
+++ EksamensProjekt/src/test/java/tests/aktivitetSteps.java
@@ -8,6 +8,9 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import Applikationslag.Data.Datavedholdelsesklasser.AktivitetData;
 import Applikationslag.Domaeneklasser.*;
+import Applikationslag.Infrastruktur.ServiceInterfaces.IAktivitetManager;
+import Applikationslag.Redskaber.Managers;
+
 import java.util.Date;
 
 public class aktivitetSteps {
@@ -17,6 +20,7 @@ public class aktivitetSteps {
 	static Date startdate;
 	String aktivitetID;
 	String projektID;
+	IAktivitetManager aktivitetManager= Managers.FaaAktivitetManager();
 	
 	
 	@Given("der er et projekt {string} med lederen {string}")
@@ -24,17 +28,43 @@ public class aktivitetSteps {
 		this.projektID=projektID;
 		leder = new Medarbejder(lederID);
 		projekt = new Projekt(projektID);
+		projekt.Gem();
 		//¯\_(ツ)_/¯
 	}
 
 	@When("lederen laver en ny aktivitet med navnet {string}")
 	public void lederenLaverEnNyAktivitetMedNavnet(String aktivitetID) {
-		this.aktivitetID=aktivitetID;
-		assertTrue(projekt.tilfoejAktivitet(this.aktivitetID));
+		aktivitet= new Aktivitet(aktivitetID);
+		assertTrue(projekt.tilfoejAktivitet(aktivitet));
 	}
 	
 	@Then("projektet {string} har en ny aktivitet med navnet {string}")
 	public void projektetHarEnNyAktivitetMedNavnet(String projektID, String aktivitetID) {
+		assertTrue(aktivitetManager.eksisterer(aktivitet));
+		assertTrue(aktivitet.getProjekt() == projekt);
+	}
+	
+	
+	// Forsoeger at tilfoeje aktivitet som allerede findes i projektet
+	@Given("Projektet {string} har aktiviteten {string}")
+	public void projektetHarAktiviteten(String ProjektID, String aktivitetID) {
+	    projekt = new Projekt(projektID);
+	    projekt.Gem();										//opret projekt
+	    aktivitet = new Aktivitet(aktivitetID);				//opret aktivitet
+	    projekt.tilfoejAktivitet(aktivitet);				//tilfoej aktivitet til projekt
+		assertTrue(aktivitetManager.eksisterer(aktivitet));	//tjek at aktiviteten findes
+		assertTrue(aktivitet.getProjekt() == projekt);		//tjek at aktivitet er i projekt
+	}
+
+	@When("lederen proever at lave en ny aktivitet med navnet {string}")
+	public void lederenProeverAtLaveEnNyAktivitetMedNavnet(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Kast exception {string}")
+	public void kastException(String string) {
+	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
 	
